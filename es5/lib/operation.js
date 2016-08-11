a595fd749e3ef6718a36a96820c3773953973275
Narazaka <info@narazaka.net>
2016-08-12 03:23:55 +0900

    named op

--- es5/lib/operation.js
+++ es5/lib/operation.js
@@ -5,6 +5,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.OperationController = exports.OperationRouting = undefined;
 
+var _promise = require('babel-runtime/core-js/promise');
+
+var _promise2 = _interopRequireDefault(_promise);
+
 var _regenerator = require('babel-runtime/regenerator');
 
 var _regenerator2 = _interopRequireDefault(_regenerator);
@@ -48,9 +52,12 @@ var OperationRouting = exports.OperationRouting = function () {
       routes.controller('OperationController', function (routes) {
         routes.from('NamedKernelManager', function (routes) {
           routes.event('start', 'boot_all');
-          routes.event('boot');
+          routes.event('boot_named');
+          routes.event('close_named');
+          routes.event('change_named');
           routes.event('close');
           routes.event('halt');
+          routes.event('kernel_unregistered');
         });
       });
     }
@@ -66,7 +73,11 @@ var OperationController = exports.OperationController = function (_NamedKernelMa
 
   function OperationController(manager) {
     (0, _classCallCheck3.default)(this, OperationController);
-    return (0, _possibleConstructorReturn3.default)(this, (0, _getPrototypeOf2.default)(OperationController).call(this, manager));
+
+    var _this = (0, _possibleConstructorReturn3.default)(this, (0, _getPrototypeOf2.default)(OperationController).call(this, manager));
+
+    _this.closeComplete = {};
+    return _this;
   }
 
   (0, _createClass3.default)(OperationController, [{
@@ -89,9 +100,7 @@ var OperationController = exports.OperationController = function (_NamedKernelMa
                 // TODO: 他形式対応
 
                 ghosts.map(function (ghostname) {
-                  return _this2.manager.loadGhost(ghostname).then(function (kernel) {
-                    return kernel.start();
-                  });
+                  return _this2.boot_named(ghostname);
                 });
 
               case 5:
@@ -108,6 +117,156 @@ var OperationController = exports.OperationController = function (_NamedKernelMa
 
       return boot_all;
     }()
+  }, {
+    key: 'boot_named',
+    value: function () {
+      var _ref2 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee2(namedId) {
+        var kernel, profile;
+        return _regenerator2.default.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.next = 2;
+                return this.manager.loadGhost(namedId);
+
+              case 2:
+                kernel = _context2.sent;
+
+                kernel.start();
+                _context2.next = 6;
+                return this.manager.profile();
+
+              case 6:
+                profile = _context2.sent;
+
+                profile.ghosts.push(namedId);
+                _context2.next = 10;
+                return this.manager.profile(profile);
+
+              case 10:
+              case 'end':
+                return _context2.stop();
+            }
+          }
+        }, _callee2, this);
+      }));
+
+      function boot_named(_x) {
+        return _ref2.apply(this, arguments);
+      }
+
+      return boot_named;
+    }()
+  }, {
+    key: 'close_named',
+    value: function close_named(namedId) {
+      var _this3 = this;
+
+      return new _promise2.default(function (resolve) {
+        var kernel = _this3.manager.kernel(namedId);
+        _this3.closeComplete[namedId] = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee3() {
+          var profile;
+          return _regenerator2.default.wrap(function _callee3$(_context3) {
+            while (1) {
+              switch (_context3.prev = _context3.next) {
+                case 0:
+                  delete _this3.closeComplete[namedId];
+                  _context3.next = 3;
+                  return _this3.manager.profile();
+
+                case 3:
+                  profile = _context3.sent;
+
+                  profile.ghosts = profile.ghosts.filter(function (ghostname) {
+                    return ghostname !== namedId;
+                  });
+                  _context3.next = 7;
+                  return _this3.manager.profile(profile);
+
+                case 7:
+                  resolve();
+
+                case 8:
+                case 'end':
+                  return _context3.stop();
+              }
+            }
+          }, _callee3, _this3);
+        }));
+        kernel.close();
+      });
+    }
+  }, {
+    key: 'change_named',
+    value: function () {
+      var _ref4 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee4(oldNamedId, newNamedId) {
+        return _regenerator2.default.wrap(function _callee4$(_context4) {
+          while (1) {
+            switch (_context4.prev = _context4.next) {
+              case 0:
+                _context4.next = 2;
+                return this.close_named(oldNamedId);
+
+              case 2:
+                _context4.next = 4;
+                return this.boot_named(newNamedId);
+
+              case 4:
+              case 'end':
+                return _context4.stop();
+            }
+          }
+        }, _callee4, this);
+      }));
+
+      function change_named(_x2, _x3) {
+        return _ref4.apply(this, arguments);
+      }
+
+      return change_named;
+    }()
+  }, {
+    key: 'close',
+    value: function () {
+      var _ref5 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee5() {
+        var _this4 = this;
+
+        return _regenerator2.default.wrap(function _callee5$(_context5) {
+          while (1) {
+            switch (_context5.prev = _context5.next) {
+              case 0:
+                _context5.next = 2;
+                return _promise2.default.all(this.manager.namedIds().map(function (namedId) {
+                  return _this4.close_named(namedId);
+                }));
+
+              case 2:
+                this.kernel.halt();
+
+              case 3:
+              case 'end':
+                return _context5.stop();
+            }
+          }
+        }, _callee5, this);
+      }));
+
+      function close() {
+        return _ref5.apply(this, arguments);
+      }
+
+      return close;
+    }()
+  }, {
+    key: 'halt',
+    value: function halt() {
+      // TODO
+    }
+  }, {
+    key: 'kernel_unregistered',
+    value: function kernel_unregistered(namedId) {
+      if (this.closeComplete[namedId]) this.closeComplete[namedId]();
+    }
   }]);
   return OperationController;
 }(_namedKernelManager.NamedKernelManagerController);
--- src/lib/operation.js
+++ src/lib/operation.js
@@ -5,9 +5,12 @@ export class OperationRouting {
     routes.controller('OperationController', (routes) => {
       routes.from('NamedKernelManager', (routes) => {
         routes.event('start', 'boot_all');
-        routes.event('boot');
+        routes.event('boot_named');
+        routes.event('close_named');
+        routes.event('change_named');
         routes.event('close');
         routes.event('halt');
+        routes.event('kernel_unregistered');
       });
     });
   }
@@ -17,13 +20,54 @@ export class OperationRouting {
 export class OperationController extends NamedKernelManagerController {
   constructor(manager) {
     super(manager);
+    this.closeComplete = {};
   }
 
   async boot_all() {
     const profile = await this.manager.profile();
     const ghosts = profile.ghosts && profile.ghosts.length ? profile.ghosts : ['js']; // TODO: 無ゴースト起動
     // TODO: 他形式対応
-    ghosts.map((ghostname) => this.manager.loadGhost(ghostname).then((kernel) => kernel.start()));
+    ghosts.map((ghostname) => this.boot_named(ghostname));
+  }
+
+  async boot_named(namedId) {
+    const kernel = await this.manager.loadGhost(namedId);
+    kernel.start();
+    const profile = await this.manager.profile();
+    profile.ghosts.push(namedId);
+    await this.manager.profile(profile);
+  }
+
+  close_named(namedId) {
+    return new Promise((resolve) => {
+      const kernel = this.manager.kernel(namedId);
+      this.closeComplete[namedId] = async () => {
+        delete this.closeComplete[namedId];
+        const profile = await this.manager.profile();
+        profile.ghosts = profile.ghosts.filter((ghostname) => ghostname !== namedId);
+        await this.manager.profile(profile);
+        resolve();
+      };
+      kernel.close();
+    });
+  }
+
+  async change_named(oldNamedId, newNamedId) {
+    await this.close_named(oldNamedId);
+    await this.boot_named(newNamedId);
+  }
+
+  async close() {
+    await Promise.all(this.manager.namedIds().map((namedId) => this.close_named(namedId)));
+    this.kernel.halt();
+  }
+
+  halt() {
+    // TODO
+  }
+
+  kernel_unregistered(namedId) {
+    if (this.closeComplete[namedId]) this.closeComplete[namedId]();
   }
 }
 
